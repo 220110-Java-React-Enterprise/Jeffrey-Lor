305463168ce08a7b18c0a686a4e216b325075d1d
jlor993 <jlor993@gmail.com>
2022-02-07 16:36:33 -0600

    Fixed Subsequence

--- assignments/Subsequence.java
+++ assignments/Subsequence.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Subsequence {
     
     public static void main(String[] args) {
-        int[] arr = {10, 9, 2, 5, 3, 7, 101, 18};
+        int[] arr = {10, 9, 0, 3, 0, 7, 101, 18};
         
         System.out.println(lengthOfLIS(arr));
     }
@@ -23,16 +23,15 @@ public class Subsequence {
         // If there are no more numbers, return what we've got
         if((nums.isEmpty())) {
             return seq.size();
-        // If the next number is less than or equal to our last inserted number, return what we've got
+        // If the next number is less than our last inserted number, return what we've got
         } else if(!seq.isEmpty() && nums.get(0) <= seq.get(seq.size() - 1)) {
-            return seq.size();
+            return subsequence(seq, nums.subList(1, nums.size()));
         }
 
-        ArrayList<Integer> seq1 = new ArrayList<>(seq);
-        ArrayList<Integer> seq2 = new ArrayList<>(seq);
-        seq2.add(nums.get(0));
+        ArrayList<Integer> newSub = new ArrayList<>(seq);
+        newSub.add(nums.get(0));
 
         // Recurse off into a path where we don't add the element, and one where we do, return the higher of the two
-        return Math.max(subsequence(seq1, nums.subList(1, nums.size())), subsequence(seq2, nums.subList(1, nums.size())));
+        return Math.max(subsequence(seq, nums.subList(1, nums.size())), subsequence(newSub, nums.subList(1, nums.size())));
     }
 }
